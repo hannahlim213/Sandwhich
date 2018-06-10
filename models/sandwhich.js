78835009778db327d7e5deab6d1a394f40ea585b
Han Ban <hannah15198@gmail.com>
2018-06-10 13:56:17 -0700

    working on appending new burgers

--- models/sandwhich.js
+++ models/sandwhich.js
@@ -2,22 +2,22 @@ var orm = require("../config/orm.js");
 
 var sandwich = {
     all: function(cb) {
-        orm.all("sandwiches", function(res){
+        orm.all("sandwich", function(res){
             cb(res);
         })
     },
     create: function(cols, vals, cb) {
-        orm.create("sandwiches", cols, vals, function(res) {
+        orm.create("sandwich", cols, vals, function(res) {
             cb(res);
         })
     },
     update: function (objColVals, condition, cb) {
-        orm.update("sandwiches", objColVals, condition, function(res){
+        orm.update("sandwich", objColVals, condition, function(res){
             cb(res);
         })
     },
     delete: function(condition, cb) {
-        orm.delete("sandwiches", condition, function (res) {
+        orm.delete("sandwich", condition, function (res) {
             cb(res);
         })
     }
